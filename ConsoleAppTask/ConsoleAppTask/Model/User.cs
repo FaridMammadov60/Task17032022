a833919259205ccddf859fd2c5fd8ce267356e06
FaridMammadov60 <faridmma@code.edu.az>
2022-03-18 04:26:26 -0700

    first commit

---
+++ ConsoleAppTask/ConsoleAppTask/Model/User.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace ConsoleAppTask.Model
+{
+    internal class User
+    {
+        #region Fields
+        int age;
+        #endregion
+
+        #region Property
+        public string Username { get; set; }
+        public string Password
+        {
+            get
+            {
+                return Password;
+            }
+            set
+            {
+                if (value.Length < 7)
+                {
+                    Console.WriteLine("Uzunluğu 8den kiçik ola bilmez");
+
+                }
+                else if (string.IsNullOrEmpty(value))
+
+                {
+                    Console.WriteLine("Boşdur");
+                }
+                else if (BigAlfb(value) == false)
+                {
+                    Console.WriteLine("Parametrler uygun deyil");
+                }
+
+            }
+
+        }
+        public int Age
+        {
+            get
+            {
+                return Age;
+            }
+
+            set
+            {
+                if (value < 0)
+                {
+                    Console.WriteLine("Yaş mənfi ola bilmez");
+
+
+                }
+                else
+                {
+                    age = value;
+                }
+            }
+        }
+        #endregion
+
+        #region Construction
+        public User(string Username, string Password)
+        {
+            this.Username = Username;
+            this.Password = Password;
+
+        }
+        #endregion
+
+
+        public override string ToString()
+        {
+            return $"User name: {Username}\n" +
+                $"User age: {Age}\n" +
+                $"User password: {Password}\n";
+        }
+
+       /// <summary>
+       /// Qebul edilmis stringi Reqem boyuk herf ve kicik herf olaraq yoxlayir
+       /// </summary>
+       /// <param name="password"></param>
+       /// <returns></returns>
+        bool BigAlfb(string password)
+        {
+            int numericCount = Regex.Matches(password, @"\d").Count;
+            int upperCount = Regex.Matches(password, @"[A-Z]").Count;
+            int lowerCount = Regex.Matches(password, @"[a-z]").Count;
+            
+            if (numericCount==0)
+            {
+                return false;
+            }
+            else if (upperCount == 0)
+            {
+                return false;
+            }
+            if (lowerCount == 0)
+            {
+                return false;
+            }
+            return true;
+        }
+
+
+}
+   
+    
+   
+}
---
+++ ConsoleAppTask/ConsoleAppTask/Program.cs
@@ -0,0 +1,26 @@
+﻿using ConsoleAppTask.Model;
+using System;
+using System.Text.RegularExpressions;
+
+namespace ConsoleAppTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Username enter: ");
+            string username=Console.ReadLine();
+            Console.WriteLine("Password enter: ");
+            string password=Console.ReadLine();
+            Console.WriteLine("Age enter: ");
+            int age=Convert.ToInt32(Console.ReadLine());    
+            
+            User user = new User(username, password)
+            {
+                Age =age
+            };
+
+            ///Console.WriteLine(user);
+        }
+    }
+}
